6191ac916f250ec9cf93ed216149c23bbe1a1558
ken-sakura <65748538+ken-sakura@users.noreply.github.com>
2023-08-22 05:59:35 +0900

    MOD project File

---
+++ testApp/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class testApp
+    {
+        public static void Main()
+        {
+            Console.Write("input filepath : ");
+            var varFilePath = Console.ReadLine();
+            if(varFilePath == ""){
+                Console.WriteLine("処理を終了します。");
+                return;
+            }
+
+            var varFileList = Directory.GetFiles(varFilePath, "*.txt");
+
+            var cntFileList = varFileList.Length;
+
+            if(cntFileList <= 0){
+                Console.WriteLine("ファイルが存在しません");
+            }else{
+                for(int icount = 0 ; icount < cntFileList ; icount++){
+                    Console.WriteLine(varFileList[icount]);
+
+                }
+
+            }
+
+            Console.WriteLine("処理終了");
+            return;
+
+
+        }
+    }
+}
